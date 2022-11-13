1cf53a095c205a32ae5e65cefcb778b4b10740a1
paulitchos <a2020121705@isec.pt>
2022-11-13 23:55:05 +0000

    merge

--- Project/Phase I/src/network.py
+++ Project/Phase I/src/network.py
@@ -7,9 +7,11 @@ class network(nn.Module):
     def __init__(self,inputSize,fc1Dim,fc2Dim):#Features, neuronios da primeira camada, neuronios da segunda camada
         super(network, self).__init__() #Devolve um objeto temporario (network)
         #inputSize é a quantidade de atributos
+
         self.fc1 = nn.Linear(inputSize,fc1Dim) #fc -> fully connected
         self.fc2 = nn.Linear(fc1Dim,fc2Dim)
-        self.out = nn.Linear(fc2Dim,1)
+        self.out = nn.Linear(fc1Dim,1)
+
 
     #
     def forward(self,x): #Função para percorrer cada camada
@@ -18,7 +20,9 @@ class network(nn.Module):
         #print(self.layer[0].get_weights())
         x = self.fc2(x)
         x = self.out(x)
-        x = torch.tanh(x) # Limitar entre -1 e 1 o output
+        #x = torch.tanh(x)
+        x = torch.softmax(x,1) # Limitar entre -1 e 1 o output
 
         return x
+
 
