4d97b973aecb8990838f39b49b471bc87395c734
Nuno Santos <nunosantos2018@hotmail.com>
2022-12-07 20:22:44 +0000

    Resutados feitos (Falta acabar o relatorio)

--- Project/Phase II/src/main.py
+++ Project/Phase II/src/main.py
@@ -20,14 +20,14 @@ from SwarmPackagePy import testFunctions as tf
 #c3 - inertica parameter
 
 
-options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+options = {'c1': 0.5, 'c2': 0.3, 'w':1.5}
 
 # ================================================================== SPHERE -> Dim = 2 ================================================================== #
 # Call instance of PSO
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)#A dimensao não é definida por nós, é a dimensao do problema
 
 # Call Firefly Algorithm
-alh = SwarmPackagePy.fa(10,tf.sphere_function, -5, 5, 2, 1000, 1, 1, 1, 0.1, 0, 0.1)
+alh = SwarmPackagePy.fa(10,tf.sphere_function, -5, 5, 2, 1000, 4, 1, 1, 0.1, 0, 0.1)
 print("\nFireFly (Sphere) [Best Pos Dim=2]: ",alh.get_Gbest()) #Retorna a melhor posição do algoritmo
 print("FireFly (Sphere) [Best Cos Dim=2]: ",tf.sphere_function(alh.get_Gbest()))
 #print("Agentes: ",alh.get_agents()) #Retorna um histórico de todos os agentes do algoritmo 
@@ -42,20 +42,21 @@ plot_cost_history(optimizer.cost_history)
 plt.show()
 
 # Plot the sphere function's mesh for better plots
-m = Mesher(func=fx.sphere,limits=[(-1,1), (-1,1)])
+#m = Mesher(func=fx.sphere,limits=[(-1,1), (-1,1)])
 
-animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
+
+#animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
 
 #Enables us to view it in a Jupyter notebook
-animation.save('Pyramid.gif',writer='imagemagick',fps=10)
-Image(url='Pyramid.gif')
+#animation.save('Pyramid.gif',writer='imagemagick',fps=10)
+#Image(url='Pyramid.gif')
 
 # ================================================================== SPHERE -> Dim = 3 ================================================================== #
 # Call instance of PSO
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=3, options=options)#A dimensao não é definida por nós, é a dimensao do problema
 
 # Call Firefly algorithm
-alh = SwarmPackagePy.fa(10,tf.sphere_function, -5, 5, 3, 1000, 1, 1, 1, 0.1, 0, 0.1)
+alh = SwarmPackagePy.fa(10,tf.sphere_function, -5, 5, 3, 1000, 4, 1, 1, 0.1, 0, 0.1)
 print("\nFireFly (Sphere) [Best Pos Dim=3]: ",alh.get_Gbest())
 print("\nFIreFly (Sphere) [Best Cost Dim=3]: ",tf.sphere_function(alh.get_Gbest()))
 #print("Agentes: ",alh.get_agents()) #Retorna um histórico de todos os agentes do algoritmo 
@@ -70,13 +71,12 @@ plot_cost_history(optimizer.cost_history)
 plt.show()
 
 # Plot the sphere function's mesh for better plots
-m = Mesher(func=fx.sphere,limits=[(-1,1), (-1,1)])
+#m = Mesher(func=fx.sphere,limits=[(-1,1), (-1,1)])
 
-animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
-
+#animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
 #Enables us to view it in a Jupyter notebook
-animation.save('Pyramid2.gif',writer='imagemagick',fps=10)
-Image(url='Pyramid2.gif')
+#animation.save('Pyramid2.gif',writer='imagemagick',fps=10)
+#Image(url='Pyramid2.gif')
 
 
 
@@ -85,16 +85,14 @@ Image(url='Pyramid2.gif')
 #c1 - cognitive parameter
 #c2 - social parameter
 #c3 - inertica parameter
-
-
-options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+options = {'c1': 0.5, 'c2': 0.5, 'w':0.2}
 
 # ================================================================== ACKLEY -> Dim = 2 ================================================================== #
 # Call instance of PSO
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)#A dimensao não é definida por nós, é a dimensao do problema
 
 # Call Firefly Algorithm
-alh = SwarmPackagePy.fa(10,tf.ackley_function, -5, 5, 2, 1000, 1, 1, 1, 0.1, 0, 0.1)
+alh = SwarmPackagePy.fa(10,tf.ackley_function, -5, 5, 2, 1000, 4, 1, 1, 0.1, 0, 0.1)
 print("\nFirefly (Ackley) [Best Pos Dim=2]: ",alh.get_Gbest()) #Retorna as cordenadas x,y
 print("\nFIreFly (Ackley) [Best Cost Dim=2]: ",tf.ackley_function(alh.get_Gbest()))
 #print("Agentes: ",alh.get_agents()) #Retorna um histórico de todos os agentes do algoritmo 
@@ -108,20 +106,20 @@ plot_cost_history(optimizer.cost_history)
 plt.show()
 
 # Plot the sphere function's mesh for better plots
-m = Mesher(func=fx.ackley,limits=[(-32,32), (-32,32)])
+#m = Mesher(func=fx.ackley,limits=[(-32,32), (-32,32)])
 
 #Make animation
-animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
+#animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
 #Enables us to view it in a Jupyter notebook
-animation.save('Pyramid3.gif',writer='imagemagick',fps=10)
-Image(url='Pyramid3.gif')
+#animation.save('Pyramid3.gif',writer='imagemagick',fps=10)
+#Image(url='Pyramid3.gif')
 
 # ================================================================== ACKLEY -> Dim = 3 ================================================================== #
 # Call instance of PSO
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=3, options=options)#A dimensao não é definida por nós, é a dimensao do problema
 
 # Call Firefly Algorithm
-alh = SwarmPackagePy.fa(10,tf.ackley_function, -5, 5, 3, 1000, 1, 1, 1, 0.1, 0, 0.1)
+alh = SwarmPackagePy.fa(10,tf.ackley_function, -5, 5, 3, 1000, 4, 1, 1, 0.1, 0, 0.1)
 print("\nFirefly (Ackley) [Best Pos Dim=3]: ",alh.get_Gbest()) #Retorna as cordenadas x,y
 print("\nFIreFly (Ackley) [Best Cost Dim=3]: ",tf.ackley_function(alh.get_Gbest()))
 #print("Agentes: ",alh.get_agents()) #Retorna um histórico de todos os agentes do algoritmo 
@@ -135,11 +133,10 @@ plot_cost_history(optimizer.cost_history)
 plt.show()
 
 # Plot the sphere function's mesh for better plots
-m = Mesher(func=fx.ackley,limits=[(-32,32), (-32,32)])
+#m = Mesher(func=fx.ackley,limits=[(-32,32), (-32,32)])
 
-
-animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
+#animation = plot_contour(pos_history=optimizer.pos_history,mesher=m,mark=(0,0))
 #Enables us to view it in a Jupyter notebook
-animation.save('Pyramid4.gif',writer='imagemagick',fps=10)
-Image(url='Pyramid4.gif')
+#animation.save('Pyramid4.gif',writer='imagemagick',fps=10)
+#Image(url='Pyramid4.gif')
 
